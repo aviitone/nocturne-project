fdaa9c5d403d274b7e793ca0128bd3cbdccc8e2d
KofiCup <axaeltheartist@gmail.com>
2023-10-27 21:37:09 +1300

    progress on hold function

--- Assets/Scripts/ObjectGrabber.cs
+++ Assets/Scripts/ObjectGrabber.cs
@@ -8,10 +8,12 @@ public class ObjectGrabber : MonoBehaviour
     private float grabDistance = 3f;
     private float throwForce = 10f;
     private Transform cameraTransform;
+    private Transform handPoint;
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
+        handPoint = cameraTransform.Find("HandPoint");
     }
 
     void Update()
@@ -25,20 +27,19 @@ public class ObjectGrabber : MonoBehaviour
                 {
                     grabbedObject = hit.collider.gameObject;
                     grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
-                    grabbedObject.transform.SetParent(transform);
-                    Transform objectToSize = transform.GetChild(0);
-                    objectToSize.SetParent(cameraTransform, false);
-                    objectToSize.localScale = Vector3.one;
-                    Transform objectToWarp = transform.GetChild(0);
-                    objectToWarp.SetParent(null);
-                    objectToWarp.SetParent(cameraTransform, false);
+                    grabbedObject.transform.SetParent(handPoint, true);
+                    grabbedObject.transform.localPosition = Vector3.zero;
+                    grabbedObject.transform.localScale = Vector3.one;
+                    grabbedObject.transform.localRotation = handPoint.localRotation;
                 }
             }
             else
             {
-                ;
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject.transform.SetParent(null);
+                grabbedObject.GetComponent<Rigidbody>().AddForce(cameraTransform.forward * throwForce, ForceMode.Impulse);
+                grabbedObject = null;
             }
         }
     }
 }
-
